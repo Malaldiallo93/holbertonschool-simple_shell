6ff9f6231027c4868dd77cebebdf94b95dc4f236
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-04-24 07:57:55 -0700

    update to the function shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,46 +1,48 @@
 #include "main.h"
-#define MAX_ARGS 1024
 
 /**
-* main - Entry point for simple shell program
-*
-* Return: Always 0
-*/
-
-
+ * main - Entry point
+ *
+ * Return: Always 0 on success.
+ */
 int main(void)
 {
-size_t len = 0;
-ssize_t nread;
-char *token;
-char *line = NULL;
-char *cmd_args[MAX_ARGS];
-int count = 0, pid = 0, status;
-while ((nread = getline(&line, &len, stdin)) != -1)
-{
-count = 0;
-token = strtok(line, " \n");
-if (token == NULL)
-continue;
-while (token != NULL)
-{
-cmd_args[count] = token;
-count++;
-token = strtok(NULL, " \n");
-}
-cmd_args[count] = NULL;
-pid = fork();
-if (pid == 0)
-{
-execvp(cmd_args[0], cmd_args);
-fprintf(stderr, "Error: command not found\n");
-exit(1);
-}
-else
-{
-wait(&status);
-}
-}
-free(line);
-return (0);
+	size_t len = 0;
+	ssize_t nread;
+	char *line = NULL;
+	char *token;
+	char *cmd_args[MAX_ARGS];
+	int count, pid, status;
+
+	while ((nread = getline(&line, &len, stdin)) != -1)
+	{
+		count = 0;
+		token = strtok(line, " \n");
+		if (token == NULL)
+			continue;
+	while (token != NULL)
+	{
+		cmd_args[count] = token;
+		count++;
+		token = strtok(NULL, " \n");
+	}
+		cmd_args[count] = NULL;
+
+	pid = fork();
+		if (pid == 0)
+		{
+			exec_path(cmd_args);
+			exit(127);
+		}
+		else
+		{
+			wait(&status);
+			free(line);
+			line = NULL;
+			len = 0;
+		}
+	}
+
+	free(line);
+	return (0);
 }
