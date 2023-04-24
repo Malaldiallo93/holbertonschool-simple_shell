9fe1a126bff7d8a9e03ff1d9089feacebba126a1
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-04-24 08:05:26 -0700

    update the main function

--- main.h
+++ main.h
@@ -9,4 +9,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+char **get_path(char **envp);
+void exec_path(char **cmd_args);
+void exit_shell(char *line);
+void env_shell(char **envp);
+
 #endif /* MAIN_H */
