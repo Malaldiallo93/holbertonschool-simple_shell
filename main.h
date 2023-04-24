4ba52fe2c0e8a45abab043ae95a6efd2721a47b6
djamal <djamaldine-mohamed@live.fr>
2023-04-23 19:32:11 -0700

    splits the code into several files

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *read_line(void);
+int parse_line(char *line, char **argv);
+
+#endif /* MAIN_H */
--- simple_shell.c
+++ simple_shell.c
@@ -1,60 +1,51 @@
-/**
- * main - Entry point for the shell program
- *
- * This function implements a simple shell that allows the user to execute
- *
- * Return: Always 0
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "main.h"
 
-#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 1024
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-char input[MAX_INPUT_SIZE];
-char *command;
+char *line;
+char *argv[MAX_ARGS];
+int status, count, pid;
 
 while (1)
 {
 printf("$ ");
-fflush(stdout);
-if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL)
-{
+line = read_line();
+if (line == NULL)
 break;
-}
-input[strcspn(input, "\n")] = 0;
-if (strcmp(input, "cd") == 0)
-{
-chdir(getenv("HOME"));
+count = parse_line(line, argv);
+if (count == 0)
 continue;
-}
-command = strtok(input, " ");
-if (command != NULL)
-{
-pid_t pid = fork();
+pid = fork();
 if (pid == -1)
 {
-fprintf(stderr, "Error creating child process\n");
-}
-else if (pid == 0)
-{
-if (execlp(command, command, (char *) NULL) == -1)
-{
-fprintf(stderr, "Command not found\n");
+perror("Erreur : échec de fork()\n");
 exit(EXIT_FAILURE);
 }
+if (pid == 0)
+{
+execvp(argv[0], argv);
+perror("Erreur : commande introuvable\n");
+exit(EXIT_FAILURE);
 }
 else
 {
-int status;
-waitpid(pid, &status, 0);
+wait(&status);
 }
 }
-}
+
+free(line);
 return (0);
 }
